81485dea7898db2d14165ef6ddb601627c2dd79c
Nitin Ahuja <nitinahuja1016@gmail.com>
2025-01-20 13:04:27 +0530

    Adding .env file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
